ad0ff0f7ec5ebae5eabae596ac4cd92015692f5d
rashawnpeters <rashawnpeters@gmail.com>
2020-02-02 00:01:16 -0500

    Commiting Russell's code work
    Russell was having issues commiting his changes, so he went it to me and I am adding it

---
+++ Group2 Project/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    public void Scene2()
+    {
+        SceneManager.LoadScene("1 Game World");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
