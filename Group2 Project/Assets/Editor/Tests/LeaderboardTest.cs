b06eaf520361663a2f59240a41c567d86ebf6606
rashawnpeters <rashawnpeters@gmail.com>
2020-02-16 01:49:33 -0500

    Updating script

---
+++ Group2 Project/Assets/Editor/Tests/LeaderboardTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using NUnit.Framework;
+
+public class LeaderboardTest 
+{
+    public Button LeaderboardButton;
+
+    [Test]
+    public void TestLeaderboardButton_ToLeaderboardScreen ()
+    {
+
+        LeaderboardButton.onClick.Invoke();
+
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        string sceneName = currentScene.name;
+
+        Assert.IsTrue(sceneName == "2 Leaderboard");
+
+
+    }
+}
