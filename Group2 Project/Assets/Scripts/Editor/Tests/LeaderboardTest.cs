787388e2c34d81aee5620d41f57aa89923ce0dd9
rashawnpeters <rashawnpeters@gmail.com>
2020-02-16 17:15:39 -0500

    Added Test

---
+++ Group2 Project/Assets/Scripts/Editor/Tests/LeaderboardTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+using NUnit.Framework;
+
+public class LeaderboardTest 
+{
+
+    [Test]
+    public void TestLeaderboardButton_ToLeaderboardScreen ()
+    {
+        Button leaderboardButton;
+
+        leaderboardButton = GameObject.Find("LeaderboardButton").GetComponent<Button>();
+
+        leaderboardButton.onClick.AddListener(() => GoToLeaderboard());
+
+        leaderboardButton.onClick.Invoke();
+
+        Scene currentScene = EditorSceneManager.GetActiveScene();
+
+        string sceneName = currentScene.name;
+
+        Assert.IsTrue(sceneName == "2 Leaderboard");
+
+    }
+
+    public void GoToLeaderboard()
+    {
+        EditorSceneManager.OpenScene("2 Leaderboard");
+    }
+}
