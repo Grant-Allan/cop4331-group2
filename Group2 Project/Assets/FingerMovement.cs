cb499240303ec9119ce629e64ce0f87e2cded5f1
yell0wjell0 <60304821+yell0wjell0@users.noreply.github.com>
2020-02-22 21:58:06 -0500

    Created Scripts and assets for first mini game

---
+++ Group2 Project/Assets/FingerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerMovement : MonoBehaviour
+{
+    public float speed = 9;
+    
+
+    void Update()
+    {
+        // When space is pressed move object to the left of the screen.
+        if (Input.GetKeyDown("space"))
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-1, 0) * speed;
+
+        // If the object reaches left boundary pull it back to the middle of the screen.
+        if (transform.position.x <= -7.95)
+        {
+            if (transform.position.x >= 0)
+                GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * speed;
+        }
+
+        // Go up if the postion is below or at the bottom boundary.
+        if (transform.position.y <= -4)
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1) * speed;
+
+        // Go down if the postion is above or at the top boundary.
+        if (transform.position.y >= 4)
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1) * speed;
+    }
+}
+
