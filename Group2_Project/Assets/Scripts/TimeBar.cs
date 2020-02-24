8e6ce273339cc47b1a556355dc1dc2042071fd3f
yell0wjell0 <60304821+yell0wjell0@users.noreply.github.com>
2020-02-23 21:18:12 -0500

    Added a place holder TimeBar

---
+++ Group2_Project/Assets/Scripts/TimeBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeBar : MonoBehaviour
+{
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Minus time from the bar
+    public void DecreaseTime(float time)
+    {
+        slider.value -= time;
+    }
+}
