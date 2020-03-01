07ce0e07062b7a848f0e900a76ef31d0f5717c9f
yell0wjell0 <60304821+yell0wjell0@users.noreply.github.com>
2020-03-01 14:27:01 -0500

    Fixed collsion detection

--- Group2_Project/Assets/Scripts/FingerMovement.cs
+++ Group2_Project/Assets/Scripts/FingerMovement.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class FingerMovement : MonoBehaviour
 {
     public float speed = 9;
+    public bool triggered = false;
 
-    void OnTriggerEnter2D(Collision2D collision)
+    void OnTriggerEnter2D()
     {
         // If the object collides pull it back to the middle of the screen.
-        //if (transform.position.x >= 0)
-        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * speed;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * (speed - 3);
+
+        triggered = true;
+
+        // Stop object in middle of screen
+        //if (transform.position.x <= 2 || transform.position.x >= -2)
+            //gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
     }
 
     void Update()
     {
+        // Stop object in middle of screen
+        if (triggered && (transform.position.x <= 0.55 && transform.position.x >= -0.55))
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+
         // When space is pressed move object to the left of the screen.
-        if (Input.GetKeyDown("space"))
+        if ((triggered == false) && Input.GetKeyDown("space"))
             GetComponent<Rigidbody2D>().velocity = new Vector2(-1, 0) * (speed + 3);
 
         // Go up if the postion is below or at the bottom boundary.
