996f0be3069d4dd74edd32aa5080b67cede35585
yell0wjell0 <60304821+yell0wjell0@users.noreply.github.com>
2020-03-01 13:48:04 -0500

    more confusing collision stuff

--- Group2_Project/Assets/Scripts/FingerMovement.cs
+++ Group2_Project/Assets/Scripts/FingerMovement.cs
@@ -6,19 +6,18 @@ public class FingerMovement : MonoBehaviour
 {
     public float speed = 9;
 
+    void OnTriggerEnter2D(Collision2D collision)
+    {
+        // If the object collides pull it back to the middle of the screen.
+        //if (transform.position.x >= 0)
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * speed;
+    }
 
     void Update()
     {
         // When space is pressed move object to the left of the screen.
         if (Input.GetKeyDown("space"))
             GetComponent<Rigidbody2D>().velocity = new Vector2(-1, 0) * (speed + 3);
-
-        // If the object reaches left boundary pull it back to the middle of the screen.
-        if (transform.position.x <= -7.95)
-        {
-            if (transform.position.x >= 0)
-                GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * speed;
-        }
 
         // Go up if the postion is below or at the bottom boundary.
         if (transform.position.y <= -4)
