c112aa178bf71a3c28de84b763c789a52d57910d
Gary Hoppenworth <garyhoppenworth@gmail.com>
2020-03-02 08:00:03 -0500

    Update StartNextGame.cs

--- Group2_Project/Assets/Scripts/StartNextGame.cs
+++ Group2_Project/Assets/Scripts/StartNextGame.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//This script class controls the transition screen
 public class StartNextGame : MonoBehaviour
 {
+    // time delay on the transition screen
     public float delay = 2.0f;
+    // next minigame (insert correct minigame here)
     public string nextGame = "DemoGameScene";
     void Start()
     {
         StartCoroutine(LoadLevelAfterDelay(delay));
     }
 
+    // enacts delay and then loads the next game
     IEnumerator LoadLevelAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
