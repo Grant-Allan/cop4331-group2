34bcb2c04a55ecc99603a6cba0435bba20a14a95
Gary Hoppenworth <garyhoppenworth@gmail.com>
2020-03-02 08:01:13 -0500

    Update ScoreUpdate.cs

--- Group2_Project/Assets/Scripts/ScoreUpdate.cs
+++ Group2_Project/Assets/Scripts/ScoreUpdate.cs
@@ -8,6 +8,7 @@ public class ScoreUpdate : MonoBehaviour
     public Text mytext = null;
     void Start()
     {
+        //stores the current score as a string variable for updating score in the transition screen
         mytext.text = PlayerStats.score.ToString();
     }
 }
