aca641e169aedde923d6a07b4792f38aafd06d78
Gary Hoppenworth <garyhoppenworth@gmail.com>
2020-03-02 08:10:08 -0500

    Update HealthUpdate.cs

--- Group2_Project/Assets/Scripts/HealthUpdate.cs
+++ Group2_Project/Assets/Scripts/HealthUpdate.cs
@@ -15,19 +15,20 @@ public class HealthUpdate : MonoBehaviour
     {
         health = PlayerStats.health;
 
-        if (health >= 1)
+        
+        if (health >= 1) // then render leftmost heart
         {
             currRenderer = heart1.GetComponent<SpriteRenderer>();
             currRenderer.sprite = heart;
         }
-
-        if (health >= 2)
+        
+        if (health >= 2) // then render middle heart
         {
             currRenderer = heart2.GetComponent<SpriteRenderer>();
             currRenderer.sprite = heart;
         }
 
-        if (health >= 3)
+        if (health >= 3) // then render rightmost heart
         {
             currRenderer = heart3.GetComponent<SpriteRenderer>();
             currRenderer.sprite = heart;
