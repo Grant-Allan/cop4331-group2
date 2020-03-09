6d005db45ece9010432cc99f7ef3151d22223029
rashawnpeters <rashawnpeters@gmail.com>
2020-03-08 20:52:26 -0400

    Added Controls to main menu

---
+++ Group2_Project/Assets/Scripts/health.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    public int current = 3;
+    public int maximum = 3;
+}
