178ceb249c7f409b566ccb903e4d23466153f7be
Gary Hoppenworth <garyhoppenworth@gmail.com>
2020-03-10 11:57:01 -0400

    fixed timing issue in Whack it! minigame

--- Group2_Project/Assets/Scripts/TimeBar.cs
+++ Group2_Project/Assets/Scripts/TimeBar.cs
@@ -9,30 +9,51 @@ public class TimeBar : MonoBehaviour
 {
     //duration of DemoGame minigame
     private float startingTime = 3.0f;
+    private float delay;
+    private bool startedWaiting;
+
     public Slider Timer;
 
     void Start()
     {
         Timer = GetComponent<Slider>();
+        startedWaiting = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        startingTime -= Time.deltaTime;
-        Timer.value = startingTime;
-
-        if (startingTime <= 0)
+        if (PlayerStats.gameDone == false)
         {
-            startingTime = 0;
-            GameResults();
+            startingTime -= Time.deltaTime;
+            Timer.value = startingTime;
         }
+
+        //minigame has ended due to time running out
+        if (startingTime <= 0 & PlayerStats.gameDone == false & startedWaiting == false)
+        {
+            startedWaiting = true;
+            startingTime = 0;
+            delay = 0.1f;
+            StartCoroutine(Wait(delay));
+
+        }
+
+        //minigame has ended due to user input
+        if (PlayerStats.gameDone & startedWaiting == false)
+        {
+            startedWaiting = true;
+            delay = 2.0f;
+            StartCoroutine(Wait(delay));
+
+        }
+
 
     }
 
     void GameResults()
     {
+        Debug.Log("should only run once");
 
         // If the player does not pass the game
         if (PlayerStats.pass == false)
@@ -47,15 +68,23 @@ public class TimeBar : MonoBehaviour
             PlayerStats.score += 1;
         }
 
+
+
         //If the Player died during this minigame
         if (PlayerStats.health == 0)
         {
             SceneManager.LoadScene("DeathScene");
         }
-
         else
         {
             SceneManager.LoadScene("TransitionScene");
         }
+    }
+
+
+    IEnumerator Wait(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GameResults();
     }
 }
--- Group2_Project/Assets/Scripts/whack.cs
+++ Group2_Project/Assets/Scripts/whack.cs
@@ -24,6 +24,7 @@ public class whack : MonoBehaviour
             {
                 PlayerStats.pass = true;
                 other.MediumWhack();
+
             }
             else
             {
