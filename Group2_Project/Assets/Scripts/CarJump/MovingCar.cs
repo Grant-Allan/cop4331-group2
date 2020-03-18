aedde7e2dc37e6498453967cd9b0fa98dd7f1889
rashawnpeters <rashawnpeters@gmail.com>
2020-03-18 17:12:49 -0400

    Starting Car Jumping game
    The heart sprite is just usered so I could test the movement. I will replace it when I make a person sprite

---
+++ Group2_Project/Assets/Scripts/CarJump/MovingCar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCar : MonoBehaviour
+{
+    public float speed = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * (speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Group2_Project/Assets/Scripts/CarJump/PersonJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonJump : MonoBehaviour
+{
+
+    public float speed = 8;
+    public float startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale = 0;
+        startPosition = transform.position.y;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // If space is pressed, make the sprite jump
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1) * (speed);
+        }
+
+        // Freeze sprite at its starting position
+        if (transform.position.y < startPosition)
+        {
+                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+        }
+    }
+}
