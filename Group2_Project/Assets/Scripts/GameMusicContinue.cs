b25bc132361c044d876de9cf533faaad0d22dd50
rashawnpeters <rashawnpeters@gmail.com>
2020-03-29 20:19:23 -0400

    Added Music and Backgrounds

---
+++ Group2_Project/Assets/Scripts/GameMusicContinue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMusicContinue : MonoBehaviour
+{
+    void Awake ()
+    {
+        GameObject[] song = GameObject.FindGameObjectsWithTag("gameMusic");
+        if (song.Length > 1)
+            Destroy(this.gameObject);
+
+        GameObject menuSong = GameObject.FindGameObjectWithTag("menuMusic");
+        Destroy(menuSong);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Group2_Project/Assets/Scripts/MenuMusicContinue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicContinue : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] song = GameObject.FindGameObjectsWithTag("menuMusic");
+        if (song.Length > 1)
+            Destroy(this.gameObject);
+
+        GameObject gameSong = GameObject.FindGameObjectWithTag("gameMusic");
+        Destroy(gameSong);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
