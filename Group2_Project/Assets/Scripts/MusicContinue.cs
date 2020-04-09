e8aa54dac1d7355ce44899030d738fe1cad1b2c5
rashawnpeters <rashawnpeters@gmail.com>
2020-04-09 19:43:48 -0400

    Adding settings and volume

---
+++ Group2_Project/Assets/Scripts/MusicContinue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicContinue : MonoBehaviour
+{
+    void Awake ()
+    {
+        GameObject[] song = GameObject.FindGameObjectsWithTag("gameMusic");
+        if (song.Length > 1)
+            Destroy(this.gameObject);
+
+        GameObject[] menuSong = GameObject.FindGameObjectsWithTag("menuMusic");
+        if (menuSong.Length > 0)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Group2_Project/Assets/Scripts/SettingsPopUpScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsPopUpScript : MonoBehaviour
+{
+    public GameObject settingsMenu;
+
+    public void Settings()
+    {
+
+        settingsMenu.SetActive(true);
+
+    }
+}
